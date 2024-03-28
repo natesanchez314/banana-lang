3be4b082b111e5585be35f7c255c1c1f9bb3bb73
Nate Sanchez <30782279+natesanchez314@users.noreply.github.com>
2024-03-28 11:07:53 -0500

    evaluate prefix expressions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -6,7 +6,7 @@ import(
 )
 
 var(
-	NULL = &object.Boolean{}
+	NULL = &object.Null{}
 	TRUE = &object.Boolean{Val: true}
 	FALSE = &object.Boolean{Val: false}
 )
@@ -14,15 +14,18 @@ var(
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	// Statements
-	case *ast.Program:
-		return evalStatements(node.Statements)
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
+	case *ast.Program:
+		return evalStatements(node.Statements)
 	// Expressions
 	case *ast.Boolean:
 		return nativeBoolToBooleanObject(node.Val)
 	case *ast.IntegerLiteral:
 		return &object.Integer{Val: node.Val}
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Op, right)
 	}
 	return nil
 }
@@ -33,6 +36,38 @@ func evalStatements(stmts []ast.Statement) object.Object {
 		res = Eval(stmt)
 	}
 	return res
+}
+
+func evalPrefixExpression(op string, right object.Object) object.Object {
+	switch op {
+	case "!":
+		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusPrefixOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
+}
+
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+	val := right.(*object.Integer).Val
+	return &object.Integer{Val: -val}
 }
 
 func nativeBoolToBooleanObject(input bool) *object.Boolean {
